3e9e3720da903ec8750d018768a0fd8902407d48
DevHWCho <User address(whgusdnd97@gmail.com)>
2023-05-18 10:47:17 +0900

    autobanner fix

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ let bnnNum = 0;
 const lastNum = slide.length - 1;
 let currentVideo;
 let vidDuration = 0;
-
 function loadVideoAndPlayNextSlide() {
   bnnNum++;
   if (bnnNum > lastNum) bnnNum = 0;
@@ -39,27 +38,25 @@ function loadVideoAndPlayNextSlide() {
   currentVideo = slide[bnnNum].querySelector("video");
   vidDuration = currentVideo.duration;
   progressBar[bnnNum].style.transition = `width ${vidDuration}s linear`;
-
-  // Play current video
-  currentVideo.play();
-
+  // 다음 슬라이드 비디오를 로드하고 미리 재생
+  const nextVideo = slide[bnnNum + 1] ? slide[bnnNum + 1].querySelector('video') : slide[0].querySelector('video');
+  nextVideo.load();
+  // nextVideo.play();
   autoMBnn = setTimeout(loadVideoAndPlayNextSlide, vidDuration * 1000);
 }
-
 function autoMainBanner() {
-  // Play first video
+  // 첫 번째 슬라이드 비디오를 로드하고 재생
   currentVideo = slide[bnnNum].querySelector("video");
   vidDuration = currentVideo.duration;
   progressBar[bnnNum].style.transition = `width ${vidDuration}s linear`;
-
-  // Play current video
+  currentVideo.load();
   currentVideo.play();
-
+  // 다음 슬라이드 비디오를 미리 로드
+  const nextVideo = slide[bnnNum + 1] ? slide[bnnNum + 1].querySelector('video') : slide[0].querySelector('video');
+  nextVideo.load();
   autoMBnn = setTimeout(loadVideoAndPlayNextSlide, vidDuration * 1000);
 }
-
 autoMainBanner();
-
 
 
 // 비디오 재생/멈춤
